75ad024c93ca4c97f01343762ce001735ef6694d
Bongani Nyoni <Bongani.Nyoni@fenergo.com>
2023-05-30 12:21:04 +0200

    first commit

---
+++ src/main/java/com/example/mavernprojectexample/MavernprojectexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavernprojectexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavernprojectexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavernprojectexampleApplication.class, args);
+	}
+
+}
